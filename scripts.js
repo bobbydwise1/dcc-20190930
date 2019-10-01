2de5bf79ccb35a2d8f62d34dc2780aa1d370b087
Robert Lee <bobbydwise1@hotmail.com>
2019-09-30 21:15:47 -0700

    Add binary tree object.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,33 @@ This problem was asked by Google.
 Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
 */
 
+class singleNode{
+  constructor(value=null,left=null,right=null) {
+    this.value=value;
+    this.left=left;
+    this.right=right;
+  }
 
+  addLeft(leftNode) {
+    this.left=leftNode
+    return this.left
+  }
+
+  addRight(rightNode) {
+    this.right=rightNode
+    return this.right
+  }
+}
+
+let root = new singleNode('a')
+let node2 = new singleNode('b')
+let node3 = new singleNode('c')
+let node4 = new singleNode('d')
+
+root.addLeft(node2)
+root.addRight(node3)
+node2.addLeft(node4)
+console.log(root)
 
 $(document).ready(function() {
   $('#form-1').submit(function(){
